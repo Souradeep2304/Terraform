aac597d89f4d43c716c74be568db558ad73e2725
Your Name <you@example.com>
2019-07-10 10:28:28 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Deploying Infrastructure as a code using Terraform
+
+Terraform is a tool for building, changing, and versioning infrastructure safely and easily. It suppoorts existing cloud service providers and custom in house solutions. Terraform makes use of configureation files in order to keep track of the components to run. Before applying the updates to the infrastructure, terraform creates a plan of the same which we can use to double check the changes before any real time changes are made using the apply command. To know more about terraform, you can go through the [`documentation of Terraform`](https://www.terraform.io/docs/index.html).
+
+## Implementing 3 tier WEB-API-DATABASE architecture using Terraform
+
+WEB-API-DATABSE architecture is a three-tier architecture which is the basic model for developing a web database application and communication is done using this 3-level application logic.</br> 
+At the base of the model lies the database tier where data gets stored in a database server and various CRUD operations are being performed here. Above the database tier lies the middle tier which contains the application logic and does all the communication between the user and the database. Lastly on the top lies the web tier which is the web client that is used to interact with the user. User puts their request through the web tier, which gets processed by the middle tier and accordingly operations are being performed on the data base tier.</br>
+
+<p align="center">
+<img src="./Figures/ARCH.png"></br>
+Figure 1.1: Working of the Architecture 
+</br></br>
+<img src="./Figures/ARCHF.jpg">
+Figure 1.2: Detailed Architecture in Azure
+</p>
+
+## How to Deploy:
+-  Install Terraform.
+- [`Configure Azure`]https://docs.microsoft.com/en-us/azure/virtual-machines/linux/terraform-install-configure) so the Terraform can connect to Azure.
+- upload the deploy.tf file in Azure powershell.
+- Type in the commands 
+    ```
+    terraform init
+    terraform plan
+    terraform apply
+    ```
---
+++ deploy.tf
@@ -0,0 +1,99 @@
+resource "azurerm_resource_group" "tera" {
+  name="TerraformRG"
+  location="East US"
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "W1"
+  location            = "East US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+}
+
+resource "azurerm_network_security_group" "nsg2" {
+  name                = "A1"
+  location            = "East US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+}
+resource "azurerm_network_security_group" "nsg3" {
+  name                = "D1"
+  location            = "East US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+}
+
+resource "azurerm_network_security_group" "nsg4" {
+  name                = "W2"
+  location            = "West US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+}
+resource "azurerm_network_security_group" "nsg5" {
+  name                = "A2"
+  location            = "West US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+}
+resource "azurerm_network_security_group" "nsg6" {
+  name                = "D2"
+  location            = "West US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "V1"
+  location            = "East US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+  address_space       = ["13.0.0.0/16"]
+
+
+  subnet {
+    name           = "S1"
+    address_prefix = "13.0.0.0/24"
+    security_group="${azurerm_network_security_group.nsg1.id}"
+  }
+   subnet {
+    name           = "S2"
+    address_prefix = "13.0.1.0/24"
+    security_group="${azurerm_network_security_group.nsg2.id}"
+  }
+   subnet {
+    name           = "S3"
+    address_prefix = "13.0.2.0/24"
+    security_group="${azurerm_network_security_group.nsg3.id}"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "V2"
+  location            = "West US"
+  resource_group_name = "${azurerm_resource_group.tera.name}"
+  address_space       = ["16.0.0.0/16"]
+
+
+  subnet {
+    name           = "S1"
+    address_prefix = "16.0.0.0/24"
+    security_group="${azurerm_network_security_group.nsg4.id}"
+  }
+   subnet {
+    name           = "S2"
+    address_prefix = "16.0.1.0/24"
+    security_group="${azurerm_network_security_group.nsg5.id}"
+  }
+   subnet {
+    name           = "S3"
+    address_prefix = "16.0.2.0/24"
+    security_group="${azurerm_network_security_group.nsg6.id}"
+  }
+}
+
+resource "azurerm_virtual_network_peering" "peer1" {
+  name                      = "V1toV2"
+  resource_group_name       = "${azurerm_resource_group.tera.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet1.name}"
+  remote_virtual_network_id = "${azurerm_virtual_network.vnet2.id}"
+}
+
+resource "azurerm_virtual_network_peering" "peer2" {
+  name                      = "V2toV1"
+  resource_group_name       = "${azurerm_resource_group.tera.name}"
+  virtual_network_name      = "${azurerm_virtual_network.vnet2.name}"
+  remote_virtual_network_id = "${azurerm_virtual_network.vnet1.id}"
+}
